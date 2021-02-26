1be16bb142a643e16bc7edec79c31712eb4e9820
Soldy <4786022+Soldy@users.noreply.github.com>
2021-02-26 08:03:45 +0000

    replace random
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const jsonfile = require('jsonfile');
 const moment = require('moment');
 const simpleGit = require('simple-git');
-const random = require('random');
+const randomInt = require('./random.js').randomInt;
 
 const FILE_PATH = './data.json';
 
 const makeCommit = n => {
     if(n===0) return simpleGit().push();
 
-    const x= random.int(0,54);
-    const y= random.int(0,6);
+    const x= randomInt(0,54);
+    const y= randomInt(0,6);
 
     const DATE = moment().subtract(1, 'y').add(1, 'd')
                     .add(x,'w').add(y,'d').format();
