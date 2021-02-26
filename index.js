2ea27bfd7bbd932ca6b5ac433995fcfca0a66786
SatYu26 <goyalsatyam8@gmail.com>
2021-02-26 18:16:36 +0530

    Modified Random.js

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-const jsonfile = require('jsonfile');
-const simpleGit = require('simple-git');
-const random = require('random');
+const jsonfile = require("jsonfile");
+const simpleGit = require("simple-git");
+const randomInt = require("./random.js").randomInt;
 
-const FILE_PATH = './data.json';
+const FILE_PATH = "./data.json";
 
-const makeCommit = n => {
-    if(n===0) return simpleGit().push();
+const makeCommit = (n) => {
+  if (n === 0) return simpleGit().push();
 
-    const x= random.int(0,54);
-    const y= random.int(0,6);
+  const x = randomInt(0, 54);
+  const y = randomInt(0, 6);
 
-    const DATE = new Date();
-    
-    DATE.setUTCFullYear(
-        DATE.getUTCFullYear()-1
-    );
-    DATE.setDate(
-        DATE.getDate()+1
-    );
-    DATE.setDate(
-        DATE.getDate()+x*7
-    );
-    DATE.setDate(
-        DATE.getDate()+y
-    );
-    /*
+  const DATE = new Date();
+
+  DATE.setUTCFullYear(DATE.getUTCFullYear() - 1);
+  DATE.setDate(DATE.getDate() + 1);
+  DATE.setDate(DATE.getDate() + x * 7);
+  DATE.setDate(DATE.getDate() + y);
+  /*
      * you can speed up with this ::
       DATE.setDate(
            DATE.getDate()+(((x*7)+y)+1)
@@ -33,22 +25,22 @@ const makeCommit = n => {
      *
      */
 
+  const data = {
+    date: DATE.valueOf(),
+  };
 
-    const data = {
-        date: DATE.valueOf()
-    }
+  console.log(DATE);
 
-    console.log(DATE);
-
-    jsonfile.writeFile(FILE_PATH, data, () => {
-        // git commit --date = ""
-        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE.toISOString() },
-        makeCommit.bind(this, --n));
-    });
-}
+  jsonfile.writeFile(FILE_PATH, data, () => {
+    // git commit --date = ""
+    simpleGit()
+      .add([FILE_PATH])
+      .commit(
+        DATE,
+        { "--date": DATE.toISOString() },
+        makeCommit.bind(this, --n)
+      );
+  });
+};
 
 makeCommit(500);
-
-
-
-
